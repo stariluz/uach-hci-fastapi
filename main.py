ed813eb0d885888a9fdf8c5cfcec1080cc26430c
Adora <im.stariluz@gmail.com>
2024-09-15 16:48:06 -0600

    Using modules to comunicate with event bus

--- main.py
+++ main.py
@@ -1,18 +1,27 @@
-from typing import Union
-
+import uvicorn
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
-
+from python_event_bus import EventBus
 
 app = FastAPI()
 
-app.mount('/static', StaticFiles(directory='static', html=True), name='static')
+app.mount('/static', StaticFiles(directory='api/static', html=True), name='static')
 
 @app.get("/")
 def read_root():
+    EventBus.call("connect", "API conected.")
+    return {"content": "Hello World"}
+
+@app.get("/button-pressed")
+def read_root():
+    EventBus.call("message", "Button pressed from API.")
     return {"content": "Hello World"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+def run():
+    config = uvicorn.Config(app, host="127.0.0.1", port=8000, log_level="info")
+    server = uvicorn.Server(config)
+    server.run()
+
+if __name__=="__main__":
+    run()
